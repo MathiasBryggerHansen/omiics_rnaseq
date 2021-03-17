5c9820920631b4c7d3be642b91a363b78a8b1687
Mathias <mathiasbh@gmail.com>
2021-03-17 08:34:56 +0100

    test

--- R/input_d.R
+++ R/input_d.R
@@ -44,7 +44,9 @@ input_d <- function(input){
     if(input$gene_filter){
       counts_data <- counts_data[apply(X = counts_data,1, function(x) var(x)!=0),] #remove zero variance genes, Warning in var(x) : NAs introduced by coercion
     }
+    print("4")
     pheno_data <- read.table(input[[paste0("phenotype",d)]][["datapath"]],header = T,stringsAsFactors = F)#read_pheno(input[[paste0("phenotype",d)]][["datapath"]]) #phenotype data is always assumed to be tabulated, the function handles some errors in read.csv
+    print(pheno_data)
     if(input$gene_id_col & input[[paste0("combined",d)]]){
       row.names(counts_data) <- make.names(counts_data[,1],unique = T)
       counts_data[,1] <- NULL
