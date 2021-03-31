c83ca32e0584e6284c7fc64544db296a0691b41f
Mathias <mathiasbh@gmail.com>
2021-03-31 10:07:44 +0200

    fix multiple search

--- R/search_Atlas.R
+++ R/search_Atlas.R
@@ -9,84 +9,67 @@
 #'
 #' @return data.frame of DE analysis
 
-search_Atlas <- function(input, atlas_id = F, probe_library = probe_library()) {#c("padj","log2FoldChange","baseMean","pvalue","t","B") from limma_analysis
+search_Atlas <- function(input, atlas_id = F, probe_library = probe_library()) {
   res <- list()
   p <- NULL
   #uses probe library to convert probes to ensembl gene id
-    if(atlas_id){
-      acc <- strsplit(gsub(input$Atlas_ids,pattern = " ",replacement = ""),split = ",")
-      allExps <- tryCatch({
-        print("trying")
-        getAtlasData(acc)
-      },
-      error=function(cond){
-        print("error")
-        showNotification("No results found for the given query",type = "message")
-        return(NULL)
-      })
-    }
-    else{
-      showNotification(paste("Contacting server... This may take a while"),type = "message")
-      # atlasRes <- searchAtlasExperiments(properties = input$atlas_query, species = input$species)
-      # tryCatch({
-      #   print("trying")
-      #   allExps <- getAtlasData(acc)
-      # },
-      # error=function(cond){
-      #   print("error")
-      #   showNotification("No results found for the given query",type = "message")
-      #   return(NULL)
-      # })
-      allExps <- tryCatch({
-        print("trying")
-        getAtlasData(acc)
-      },
-      error=function(cond){
-        print("error")
-        showNotification("No results found for the given query",type = "message")
-        return(NULL)
-      })
-    }
+  if(atlas_id){
+    acc <- unlist(strsplit(gsub(input$Atlas_ids,pattern = " ",replacement = ""),split = ","))
+    print(acc)
+    allExps <- tryCatch({
+      getAtlasData(acc)
+    },
+    error=function(cond){
+      showNotification("No results found for the given query",type = "message")
+      return(NULL)
+    })
+  }
+  else{
+    showNotification(paste("Contacting server... This may take a while"),type = "message")
+    allExps <- tryCatch({
+      getAtlasData(acc)
+    },
+    error=function(cond){
+      showNotification("No results found for the given query",type = "message")
+      return(NULL)
+    })
+  }
   if(is.null(allExps)){
     return(NULL)
   }
-    print("continuing")
-    for(fi in names(allExps)){
-      try({
-        showNotification(paste("Analyzing",fi),type = "message")
-        #p <- "control|normal|reference"
-        type <- names(allExps[[fi]])
-        temp <- allExps[[fi]][[1]]
-        pheno <- temp$AtlasAssayGroup #this gets very messy -> gsub(paste0(temp$AtlasAssayGroup,temp$clinical_information,temp$disease),pattern = " ",replacement = ".") #AtlasAssayGroup should be a general ID
-        if(length(unique(pheno))==1|length(unique(pheno))>8|length(pheno)==length(unique(pheno))){
-          next
+  for(fi in names(allExps)){
+    try({
+      showNotification(paste("Analyzing",fi),type = "message")
+      type <- names(allExps[[fi]])
+      temp <- allExps[[fi]][[1]]
+      pheno <- temp$AtlasAssayGroup #this gets very messy -> gsub(paste0(temp$AtlasAssayGroup,temp$clinical_information,temp$disease),pattern = " ",replacement = ".") #AtlasAssayGroup should be a general ID
+      if(length(unique(pheno))==1|length(unique(pheno))>8|length(pheno)==length(unique(pheno))){
+        next
+      }
+      if(grepl(type,"rnaseq")){#Data from atlas is either raw RNAseq counts or normalized array data
+        d0 <- DGEList(assays(temp)$counts)
+        if(input$gene_filter){
+          d0 <- d0[filterByExpr(d0, group=pheno),, keep.lib.sizes=FALSE]
         }
-        if(grepl(type,"rnaseq")){#Data from atlas is either raw RNAseq counts or normalized array data
+        d0 <- calcNormFactors(d0, method = "TMM")# #TMM normalization #assays(counts) - only from atlas
+        f <- factor(pheno, levels=unique(pheno))
 
-          d0 <- DGEList(assays(temp)$counts)
-          if(input$gene_filter){
-            d0 <- d0[filterByExpr(d0, group=pheno),, keep.lib.sizes=FALSE]
-          }
-          d0 <- calcNormFactors(d0, method = "TMM")# #TMM normalization #assays(counts) - only from atlas
-          #counts <- cpm(counts,log = T)
-          f <- factor(pheno, levels=unique(pheno))#phenotypes[[1]]
-
-          mm <- model.matrix(~0+f)
-          temp <- voom(d0, mm)
-        }
-        else{
-          temp <- exprs(temp)
-        }
-        p_id <- gsub(fi,pattern = "-",replacement = "_")
-        ids <- row.names(temp)
-        if(!grepl(ids[1],pattern = "ENS")){
-          ids <- probe_library$ensembl_gene_id[match(x = ids, probe_library$probe)]
-        }
-        row.names(temp) <- make.names(ids,unique = T)
-        res[[p_id]] <- limma_analysis(countdata = temp,phenotypes = pheno, control = input$control1, auto = T)$test
-      })
-    }
-    ##clean up
-    remove(allExps)
+        mm <- model.matrix(~0+f)
+        temp <- voom(d0, mm)
+      }
+      else{
+        temp <- exprs(temp)
+      }
+      p_id <- gsub(fi,pattern = "-",replacement = "_")
+      ids <- row.names(temp)
+      if(!grepl(ids[1],pattern = "ENS")){
+        ids <- probe_library$ensembl_gene_id[match(x = ids, probe_library$probe)]
+      }
+      row.names(temp) <- make.names(ids,unique = T)
+      res[[p_id]] <- limma_analysis(countdata = temp,phenotypes = pheno, control = input$control1, auto = T)$test
+    })
+  }
+  ##clean up
+  remove(allExps)
   return(res)
 }
