5d27605cea968b364bba181d5a7f00efead97d94
Mathias <mathiasbh@gmail.com>
2021-04-06 13:01:11 +0200

    f

--- R/readFile.R
+++ R/readFile.R
@@ -1,6 +1,7 @@
 readFile <- function(f) {
   pheno_data <- tryCatch({
-  read.table(f,header = T,stringsAsFactors = F)},#read_pheno(input[[paste0("phenotype",d)]][["datapath"]]) #phenotype data is always assumed to be tabulated, the function handles some errors in read.csv
+    print(f)
+  read.table(file = toString(f),header = T,stringsAsFactors = F)},#read_pheno(input[[paste0("phenotype",d)]][["datapath"]]) #phenotype data is always assumed to be tabulated, the function handles some errors in read.csv
   warning = function(cond) {
     showNotification("The count file could not be read returning:",type = "message")
     showNotification(cond,type = "message")
