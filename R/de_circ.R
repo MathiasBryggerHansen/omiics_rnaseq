169d8ec80f7c8a92b5ea45080b4e134937cccf54
Mathias <mathiasbh@gmail.com>
2021-04-08 09:27:40 +0200

    f

--- R/de_circ.R
+++ R/de_circ.R
@@ -37,8 +37,8 @@ de_circ <- function(input, data, pheno, ensembl2id, i){
     row.names(data) <- paste(data$Internal_circRNA_ID, data$gene_symbol, sep = "_")
     colnames(data) = gsub(pattern = ".CIRI2.circRNAs.txt_BSJ|.CIRI2.circRNAs.txt_LIN", "", colnames(data))
     print("de_circ32")
-    junction_data <- data[,junctions][,seq(1,length(p),2)]
-    linear_data <- data[,linear][,seq(1,length(p),2)]
+    junction_data <- data[,junctions]
+    linear_data <- data[,linear]
     data$sum_lin <- apply(linear_data,1, FUN = sum)
     data$sum_junction <- data$Total_BSJ
   }
